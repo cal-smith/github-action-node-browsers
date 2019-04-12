8c483e4f7be4d26f1fe6070fb2f4fcea8fed33f0
Cal Smith <calsmith@protonmail.com>
2019-04-12 14:06:38 -0400

    set chrome_bin and chrome_path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 # start our virtual framebuffer
 Xvfb :99 -ac &
 
+export CHROME_BIN=/usr/bin/chromium-browser
+export CHROME_PATH=/usr/lib/chromium/
+
 # run chrome in headless mode
 chromium-browser --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
 
