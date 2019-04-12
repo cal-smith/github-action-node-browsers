6daf42ad15c14ad15e2544575052cac1d73cdf94
Cal Smith <calsmith@protonmail.com>
2019-04-12 14:13:45 -0400

    add --no-sandbox so we can run as root

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ export CHROME_BIN=/usr/bin/chromium-browser
 export CHROME_PATH=/usr/lib/chromium/
 
 # run chrome in headless mode
-chromium-browser --headless --disable-gpu --remote-debugging-port=9222 http://localhost &
+chromium-browser --headless --disable-gpu --no-sandbox --remote-debugging-port=9222 http://localhost &
 
 # execute arguments
 sh -c "npm $*"
